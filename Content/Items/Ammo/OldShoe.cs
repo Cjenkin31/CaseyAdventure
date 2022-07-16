ffa1bb3c61d56f04e3c4a2a32e43eecaae7711c8
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-07-16 19:25:55 -0400

    Added a Shoe Gun This gun shoots Old Shoes at enemies

---
+++ Content/Items/Ammo/OldShoe.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CaseyAdventure.Content.Items.Ammo
+{
+	public class OldShoe : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Spinning Shoe");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 14;
+			Item.height = 14;
+
+			Item.damage = 8;
+			Item.DamageType = DamageClass.Ranged;
+
+			Item.maxStack = 999;
+			Item.consumable = true;
+			Item.knockBack = 2f;
+			Item.value = Item.sellPrice(0, 0, 1, 0);
+			Item.rare = ItemRarityID.Yellow;
+			Item.shoot = ModContent.ProjectileType<Projectiles.ShoeProjectile>(); // The projectile that weapons fire when using this item as ammunition.
+
+			Item.ammo = Item.type; // Important. The first item in an ammo class sets the AmmoID to its type
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		// Here we create recipe for 999/ExampleCustomAmmo stack from 1/ExampleItem
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.OldShoe, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
---
+++ Content/Items/ShoeGun.cs
@@ -0,0 +1,49 @@
+﻿using CaseyAdventure.Content.Dusts;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CaseyAdventure.Content.Items.Ammo;
+namespace CaseyAdventure.Content.Items
+{
+    class ShoeGun : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shoe Gun");
+            Tooltip.SetDefault("shoots your ditry shoes at your enemies.");
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 10;
+            Item.height = 5;
+
+            Item.autoReuse = true;
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Ranged;
+            Item.knockBack = 4f;
+            Item.noMelee = true;
+            Item.rare = ItemRarityID.Yellow;
+            Item.shootSpeed = 10f;
+            Item.useAnimation = 20;
+            Item.useTime = 20; // The item's use time in ticks (60 ticks == 1 second.)
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.value = Item.buyPrice(gold: 1);
+            Item.shoot = ModContent.ProjectileType<Projectiles.ShoeProjectile>();
+            Item.useAmmo = ModContent.ItemType<OldShoe>();
+        }
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 0);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}
