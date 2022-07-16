6f53f0a428659cf9b7bfe0b3d8cb53c29b6cf2aa
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-07-15 21:44:16 -0400

    Added a action file... ...This is where all of my action related stuff will go

---
+++ ActionFunctions.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+
+namespace CaseyAdventure
+{
+    class ActionFunctions
+    {
+		public static bool BlockInWay(int tilex, int tiley) {
+			return !(Collision.EmptyTile(tilex, tiley, false));
+		}
+		public static bool IsOnGround(int tilex, int tiley) {
+			return !(Collision.EmptyTile(tilex, tiley+1, false));
+		}
+
+        public static bool build(int tilex,int tiley)
+		{
+			if(!IsOnGround(tilex,tiley)){
+				return true;
+			}
+					int y=tiley;
+					while(y != 0)
+					{
+						if (BlockInWay(tilex, y)){
+							break;
+						}
+						int cx = tilex;
+						int cy = y;
+						WorldGen.PlaceTile(cx, cy, TileID.VineRope);
+						y--;
+					}
+					return true;
+		}
+    }
+}
