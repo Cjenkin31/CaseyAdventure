360f7a31ec4e295a097b86cffe09f1ab331fe71a
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-07-18 20:06:03 -0400

    added an item to find islands

---
+++ Content/Items/IslandFinder.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CaseyAdventure.Content.Items
+{
+    class IslandFinder : Terraria.ModLoader.ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Island Finder");
+            Tooltip.SetDefault("Tells you how far your X and Y is away from the island");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = 1;
+            Item.useTurn = true;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.useStyle = ItemUseStyleID.RaiseLamp;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.whoAmI != Main.LocalPlayer.whoAmI)
+            {
+                return true;
+            }
+
+            Vector2 playerPosition = player.position;
+            int playerX = (int)(playerPosition.X / 16f);
+            int playerY = (int)(playerPosition.Y / 16f);
+
+            ActionFunctions.Find(playerX, playerY);
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 0);
+            recipe.Register();
+        }
+    }
+}
