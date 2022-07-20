dfa54e21d8684898640d1d692f05d91bfaaea4f8
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-07-19 23:38:10 -0400

    added a baseball and baseball bat

---
+++ Content/Items/BaseballBat.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CaseyAdventure.Content.Projectiles;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+namespace CaseyAdventure.Content.Items
+{
+	public class BaseballBat : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Baseball Bat"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Shoots a baseball that you can reflect to deal more damage.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 50;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 60;
+			Item.useAnimation = 20;
+			Item.useStyle = 1;
+			Item.knockBack = 6;
+			Item.value = 10000;
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+			Item.shoot = ModContent.ProjectileType<Baseball>();
+			Item.shootSpeed = 5;
+		}
+        public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.Bone, 20);
+			recipe.AddIngredient(ItemID.SandBlock, 5);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+	}
+}
---
+++ Content/Projectiles/Baseball.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using CaseyAdventure.Content.Dusts;
+using Terraria.DataStructures;
+using System;
+using System.Linq;
+using CaseyAdventure.Content.Items;
+namespace CaseyAdventure.Content.Projectiles
+{
+	public class Baseball : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Baseball");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 8;
+			Projectile.height = 8;
+			Projectile.aiStyle = 1;
+			Projectile.friendly = true; // Can the projectile deal damage to enemies?
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.penetrate = 100;
+			Projectile.timeLeft = 1000;
+			Projectile.alpha = 255;
+			Projectile.ignoreWater = true;
+			Projectile.tileCollide = true;
+			Projectile.extraUpdates = 1;
+			Projectile.scale = 1f;
+			AIType = ProjectileID.Bullet;
+
+		}
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			Projectile.penetrate--;
+			if (Projectile.penetrate <= 0)
+			{
+				Projectile.Kill();
+			}
+			else
+			{
+				Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+				SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+
+				// If the projectile hits the left or right side of the tile, reverse the X velocity
+
+				if (Math.Abs(Projectile.velocity.X - oldVelocity.X) > float.Epsilon)
+				{
+					Projectile.velocity.X = -oldVelocity.X;
+				}
+
+				// If the projectile hits the top or bottom side of the tile, reverse the Y velocity
+				if (Math.Abs(Projectile.velocity.Y - oldVelocity.Y) > float.Epsilon)
+				{
+					Projectile.velocity.Y = -oldVelocity.Y;
+				}
+			}
+
+			return false;
+		}
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+			Projectile.velocity.X = -Projectile.velocity.X;
+			Projectile.velocity.Y = -Projectile.velocity.Y;
+			base.OnHitNPC(target, damage, knockback, crit);
+        }
+        public override void AI()
+		{
+			int rdm = Main.rand.Next(1, 100);
+			Projectile.rotation += Projectile.velocity.ToRotation() * rdm;
+			
+			Player player = Main.player.FirstOrDefault();
+			int distanceFromPlayerX = (int)(Projectile.position.X - player.position.X);
+			int distanceFromPlayerY = (int)(Projectile.position.Y - player.position.Y);
+			Projectile.velocity.X -= 1 / Projectile.timeLeft;
+			if (distanceFromPlayerX < 20 && distanceFromPlayerX > -20 && distanceFromPlayerY < 20 && distanceFromPlayerY > -20) 
+            {
+				
+				if (player.ItemAnimationActive && Projectile.timeLeft<900 && player.HeldItem.type == ModContent.ItemType<BaseballBat>())
+                {
+					Projectile.velocity.X*=(float)-1.0001;
+					Projectile.velocity.Y *= (float)-1.0001;
+					Projectile.timeLeft += 10;
+					Projectile.damage += 5;
+
+				}
+            }
+        }
+	}
+}
